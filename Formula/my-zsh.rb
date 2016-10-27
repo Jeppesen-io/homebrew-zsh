f0836d5e778529fc8ca830b2a866e552d3a12b9e
Nelson Jeppesen <Nelson@SimplyMeasured.com>
2016-10-27 14:25:47 -0700

    First release of my zsh install

---
+++ Formula/my-zsh.rb
@@ -0,0 +1,25 @@
+class MyZsh < Formula
+  desc 'Install Jeppesens zsh'
+  url 'https://github.com/Jeppesen-io/homebrew-zsh/archive/master.zip'
+  homepage 'https://github.com/Jeppesen-io/homebrew-zsh'
+  version '1.3.1'
+
+  bottle :unneeded
+
+  depends_on 'antigen'
+  depends_on 'git'
+  depends_on 'zsh'
+
+  def install
+
+    # Get home dir (stripped by homebrew)
+    home_dir = `sudo -Hu $USER bash -c 'echo -n $HOME'`
+
+    system 'cp', '-v',  "./dot-files/zshrc",  "#{prefix}/zshrc"
+    system 'ln', '-fs', "#{prefix}/zshrc",  "#{home_dir}/.zshrc"
+
+    `chsh -s /bin/zsh`
+
+  end
+
+end
