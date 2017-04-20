689983ffafe8cb86a3b48278b543d5f9541c093a
Nelson Jeppesen <nelson@simplymeasured.com>
2017-04-20 12:21:34 -0700

    Update var for syntax highlighting

--- plugins/syntax-highlighting.zsh
+++ plugins/syntax-highlighting.zsh
@@ -2,7 +2,7 @@
 # Loading now takes under 100ms
 local plugin_name='zsh-syntax-highlighting'
 local plugin_git='https://github.com/zsh-users/zsh-syntax-highlighting.git'
-local plugin_init='$plugin_name.zsh'
+local plugin_init="$plugin_name.zsh"
 
 local plugin_home="$ZSH_CACHE_HOME/$plugin_name"
 
