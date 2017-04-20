10acb5aee94c8a4a54be4ef72258fbf0139df23f
Nelson Jeppesen <nelson@simplymeasured.com>
2017-04-20 12:20:55 -0700

    Update syntax highlighting

--- plugins/syntax-highlighting.zsh
+++ plugins/syntax-highlighting.zsh
@@ -1,9 +1,13 @@
 # Install directly because I found ZSH plugin managers too slow
 # Loading now takes under 100ms
 local plugin_name='zsh-syntax-highlighting'
-local plugin_home=$ZSH_CACHE_HOME/$plugin_name
+local plugin_git='https://github.com/zsh-users/zsh-syntax-highlighting.git'
+local plugin_init='$plugin_name.zsh'
+
+local plugin_home="$ZSH_CACHE_HOME/$plugin_name"
+
 if [ ! -d $plugin_home_home ] ; then
- git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $plugin_home
+ git clone $plugin_git $plugin_home
 fi
 
-source  $plugin_name/$plugin_name.zsh
+source  $plugin_home/$plugin_init
