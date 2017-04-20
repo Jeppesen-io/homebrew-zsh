24dab6594f1743ac558e197c22c64f444ecedda3
Nelson Jeppesen <nelson@simplymeasured.com>
2017-04-20 10:40:36 -0700

    Speed up rbenv loading times

--- plugins/rbenv.zsh
+++ plugins/rbenv.zsh
@@ -1,3 +1,4 @@
 export_and_mkdir RBENV_ROOT "$XDG_CACHE_HOME/rbenv"
-eval "$(rbenv init -)"
-#echo 2.4.0 > ~/.ruby-version
+
+# Setup rbenv ; don't rehas to speed things up by 100ms or more
+eval "$(rbenv init - --no-rehash)"
