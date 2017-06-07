6b4644d944d81e22d08d1c07b7e2a899c0ba6379
Nelson Jeppesen <nelson@simplymeasured.com>
2017-06-07 13:00:34 -0700

    Remove spotify; too slow

--- plugins/powerlevel9k.zsh
+++ plugins/powerlevel9k.zsh
@@ -14,27 +14,10 @@ function () {
   export POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=''
   export POWERLEVEL9K_MULTILINE_SECOND_PROMPT_PREFIX='$ '
   export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs status)
-  export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(custom_spotify_song todo load)
-
-  # Custom status for playing Spotify song
-  export POWERLEVEL9K_CUSTOM_SPOTIFY_SONG="echo \$(prompt_spotify_song)"
-  export POWERLEVEL9K_CUSTOM_SPOTIFY_SONG_BACKGROUND="blue"
-  export POWERLEVEL9K_CUSTOM_SPOTIFY_SONG_FOREGROUND="yellow"
-  prompt_spotify_song () {
-    state=`osascript -e 'tell application "Spotify" to player state as string'`;
-    if [ $state = "playing" ]; then
-      artist=`osascript -e 'tell application "Spotify" to artist of current track as string'`;
-      track=`osascript -e 'tell application "Spotify" to name of current track as string'`;
-
-      echo -n "$artist - $track "
-    fi
-  }
-
+  export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(load)
 
   # Install directly because I found ZSH plugin managers too slow
   # Loading now takes under 100ms
-
-
   local plugin_home="$ZSH_CACHE_HOME/$plugin_name"
 
   if [ ! -d $plugin_home ] ; then
