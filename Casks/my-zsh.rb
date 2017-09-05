d85bca4b1e771e36caaac079245596e11e06d6ef
Nelson Jeppesen <nelson@Nelsons-MacBook.local>
2017-09-04 22:50:43 -0700

    Move to Homebrew's Casks so I can access $HOME

---
+++ Casks/my-zsh.rb
@@ -0,0 +1,22 @@
+cask 'my-zsh' do
+
+  version '1.0.2'
+  url 'https://github.com/Jeppesen-io/homebrew-zsh/archive/master.zip'
+  homepage 'https://github.com/Jeppesen-io/homebrew-zsh'
+  sha256 :no_check
+
+  artifact 'homebrew-zsh-master/dot-files/zshrc',   target: "#{ENV['HOME']}/.zshrc"
+
+  depends_on formula: 'zsh'
+
+  preflight do
+    `rm -v #{ENV['HOME']}/.zshrc`
+  end
+
+  postflight do
+
+    `chsh -s /usr/local/bin/zsh`
+
+  end
+
+end
