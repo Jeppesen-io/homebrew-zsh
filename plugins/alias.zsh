948ce509e7367093aeb096658338e0ea4caf83c2
Nelson Jeppesen <Nelson>
2017-09-06 11:26:09 -0700

    Add my-casks updater alias

--- plugins/alias.zsh
+++ plugins/alias.zsh
@@ -1,4 +1,5 @@
 # Update stuff
+alias update-my-casks='brew update ; brew cask list | egrep "^my-" | xargs brew cask reinstall'
 alias update-brew='brew update -v && brew upgrade -v && brew cleanup && brew cask cleanup && brew prune'
 alias update-osx='softwareupdate --install --all --verbose'
 
