bfc67ea32d51cdfc961891c6e4fb6bdeea2dad8e
Nelson Jeppesen <Nelson>
2017-09-20 10:39:29 -0700

    Add pwd in iTerm tab title

--- plugins/powerlevel9k.zsh
+++ plugins/powerlevel9k.zsh
@@ -8,13 +8,17 @@ function () {
   export POWERLEVEL9K_FAIL_ICON=🔥
   export POWERLEVEL9K_STATUS_VERBOSE=false
 
+  # Show the directory in iTerm2 titlebar
+  export POWERLEVEL9K_CUSTOM_TITLE='echo -ne "\033]0;$(dirs)\007"'
+
   # The only theme you need!
   export POWERLEVEL9K_MODE='awesome-patched'
   export POWERLEVEL9K_PROMPT_ON_NEWLINE=true
   export POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=''
   export POWERLEVEL9K_MULTILINE_SECOND_PROMPT_PREFIX='$ '
-  export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs status)
-  export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(load)
+  export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(custom_title context dir vcs status)
+  export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(load )
+
 
   # Install directly because I found ZSH plugin managers too slow
   # Loading now takes under 100ms
